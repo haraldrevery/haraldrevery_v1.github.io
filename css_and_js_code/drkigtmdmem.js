a73d7f349b5c654f47b3fd1a6a97bb047558a372
H <HaraldRevery@proton.me>
2026-01-18 16:39:26 +0100

    Delete css_and_js_code/drkigtmdmem.js

--- css_and_js_code/drkigtmdmem.js
+++
@@ -1,8 +0,0 @@
-// Remember darkmode/lightmode
-<script>
-  const isDark = localStorage.getItem('darkMode') === 'true' || localStorage.getItem('darkMode') === null;
-  if (isDark) {
-    document.documentElement.classList.add('dark');
-  } else {
-    document.documentElement.classList.remove('dark');
-  }
